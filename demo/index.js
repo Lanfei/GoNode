1b5471370d12b82c8249565a4bfeebfb6df76b27
Lanfei <CooLanfei@163.com>
2015-12-09 15:39:30 +0800

    Using the `async` module to resolve callbacks

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,12 @@
 var http = require('http');
 var App = require('../');
 
+const PORT = 8000;
+
 var server = http.createServer(new App({
 	root: __dirname + '/statics'
 }));
 
-server.listen(8000);
+server.listen(PORT, function () {
+	console.log('Listening on', PORT);
+});
